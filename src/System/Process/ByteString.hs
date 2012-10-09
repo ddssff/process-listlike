15f54549dbb3fd0171580bd20e9aa011929c4576
David Fox <dsf@seereason.com>
2012-10-09 19:34:03 +0000

    Add a strict version of readModifiedProcess

--- src/System/Process/ByteString.hs
+++ src/System/Process/ByteString.hs
@@ -55,3 +55,45 @@ readModifiedProcessWithExitCode modify cmd args input = mask $ \restore -> do
       ex <- waitForProcess pid
 
       return (ex, out, err)
+
+readProcess
+    :: FilePath                 -- ^ command to run
+    -> [String]                 -- ^ any arguments
+    -> ByteString               -- ^ standard input
+    -> IO ByteString            -- ^ exitcode, stdout, stderr
+readProcess = readModifiedProcess id
+
+-- | Like 'System.Process.readProcessWithExitCode', but using 'ByteString'
+readModifiedProcess
+    :: (CreateProcess -> CreateProcess)
+                                -- ^ Modify CreateProcess with this
+    -> FilePath                 -- ^ command to run
+    -> [String]                 -- ^ any arguments
+    -> ByteString               -- ^ standard input
+    -> IO ByteString            -- ^ exitcode, stdout, stderr
+readModifiedProcess modify cmd args input = mask $ \restore -> do
+    let modify' p = modify (p {std_in = CreatePipe, std_out = CreatePipe, std_err = Inherit })
+
+    (Just inh, Just outh, Just errh, pid) <-
+        createProcess (modify' (proc cmd args))
+
+    flip onException
+      (do hClose inh; hClose outh; hClose errh;
+          terminateProcess pid; waitForProcess pid) $ restore $ do
+
+      -- fork off a thread to start consuming stdout
+      waitOut <- forkWait $ B.hGetContents outh
+
+      -- now write and flush any input
+      unless (B.null input) $ do B.hPutStr inh input; hFlush inh
+      hClose inh -- done with stdin
+
+      -- wait on the output
+      out <- waitOut
+
+      hClose outh
+
+      -- wait on the process
+      ex <- waitForProcess pid
+
+      return out
