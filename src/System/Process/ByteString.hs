f375c687bd7d369b3c83a5e3d219785635575b59
David Fox <dsf@seereason.com>
2012-10-11 12:49:05 +0000

    Similar changes for non-lazy versions

--- src/System/Process/ByteString.hs
+++ src/System/Process/ByteString.hs
@@ -4,10 +4,11 @@ import Control.Exception
 import Control.Monad
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
+import Prelude hiding (catch)
 import System.Process
-import System.Exit (ExitCode)
+import System.Exit (ExitCode(ExitSuccess, ExitFailure))
 import System.IO
-import Utils (forkWait)
+import Utils (forkWait, resourceVanished, mkError)
 
 readProcessWithExitCode
     :: FilePath                 -- ^ command to run
@@ -55,3 +56,48 @@ readModifiedProcessWithExitCode modify cmd args input = mask $ \restore -> do
       ex <- waitForProcess pid
 
       return (ex, out, err)
+
+readProcess
+    :: FilePath                 -- ^ command to run
+    -> [String]                 -- ^ any arguments
+    -> ByteString               -- ^ standard input
+    -> IO ByteString            -- ^ stdout
+readProcess = readModifiedProcess id
+
+-- | Like 'System.Process.readProcessWithExitCode', but using 'ByteString'
+readModifiedProcess
+    :: (CreateProcess -> CreateProcess)
+                                -- ^ Modify CreateProcess with this
+    -> FilePath                 -- ^ command to run
+    -> [String]                 -- ^ any arguments
+    -> ByteString               -- ^ standard input
+    -> IO ByteString            -- ^ stdout
+readModifiedProcess modify cmd args input = mask $ \restore -> do
+    let modify' p = (modify p) {std_in = CreatePipe, std_out = CreatePipe, std_err = Inherit }
+
+    (Just inh, Just outh, _, pid) <-
+        createProcess (modify' (proc cmd args))
+
+    flip onException
+      (do hClose inh; hClose outh;
+          terminateProcess pid; waitForProcess pid) $ restore $ do
+
+      -- fork off a thread to start consuming stdout
+      waitOut <- forkWait $ B.hGetContents outh
+
+      -- now write and flush any input
+      unless (B.null input) $ (do B.hPutStr inh input
+                                  hFlush inh
+                                  hClose inh) `catch` resourceVanished
+
+      -- wait on the output
+      out <- waitOut
+
+      hClose outh
+
+      -- wait on the process
+      ex <- waitForProcess pid
+
+      case ex of
+        ExitSuccess   -> return out
+        ExitFailure r -> ioError (mkError cmd args r)
--- src/System/Process/Text.hs
+++ src/System/Process/Text.hs
@@ -5,10 +5,11 @@ import Control.Monad
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import Prelude hiding (catch)
 import System.Process
-import System.Exit (ExitCode)
+import System.Exit (ExitCode(ExitSuccess, ExitFailure))
 import System.IO
-import Utils (forkWait)
+import Utils (forkWait, resourceVanished, mkError)
 
 readProcessWithExitCode
     :: FilePath                 -- ^ command to run
@@ -56,3 +57,48 @@ readModifiedProcessWithExitCode modify cmd args input = mask $ \restore -> do
       ex <- waitForProcess pid
 
       return (ex, out, err)
+
+readProcess
+    :: FilePath                 -- ^ command to run
+    -> [String]                 -- ^ any arguments
+    -> Text                     -- ^ standard input
+    -> IO Text                  -- ^ stdout
+readProcess = readModifiedProcess id
+
+-- | Like 'System.Process.readProcess', but using 'Text'
+readModifiedProcess
+    :: (CreateProcess -> CreateProcess)
+                                -- ^ Modify CreateProcess with this
+    -> FilePath                 -- ^ command to run
+    -> [String]                 -- ^ any arguments
+    -> Text                     -- ^ standard input
+    -> IO Text                  -- ^ stdout
+readModifiedProcess modify cmd args input = mask $ \restore -> do
+    let modify' p = (modify p) {std_in = CreatePipe, std_out = CreatePipe, std_err = Inherit }
+
+    (Just inh, Just outh, _, pid) <-
+        createProcess (modify' (proc cmd args))
+
+    flip onException
+      (do hClose inh; hClose outh;
+          terminateProcess pid; waitForProcess pid) $ restore $ do
+
+      -- fork off a thread to start consuming stdout
+      waitOut <- forkWait $ T.hGetContents outh
+
+      -- now write and flush any input
+      unless (T.null input) $ (do T.hPutStr inh input
+                                  hFlush inh
+                                  hClose inh) `catch` resourceVanished
+
+      -- wait on the output
+      out <- waitOut
+
+      hClose outh
+
+      -- wait on the process
+      ex <- waitForProcess pid
+
+      case ex of
+        ExitSuccess   -> return out
+        ExitFailure r -> ioError (mkError cmd args r)
