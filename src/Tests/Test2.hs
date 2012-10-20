67079bbf361c8b81a0ec4c2e63ffa15236805caa
David Fox <dsf@seereason.com>
2012-10-20 15:07:07 +0000

    Update Test2.hs

--- src/Tests/Test2.hs
+++ src/Tests/Test2.hs
@@ -1,7 +1,19 @@
 #!/usr/bin/runhaskell
 
-import System.Process.String (readProcessWithExitCode)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as L
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as LT
+import System.Process.Read (readProcessWithExitCode)
 
 main =
-    do (code, out, err) <- readProcessWithExitCode "./Test1.hs" [] ""
+    do (code, out, err) <- readProcessWithExitCode "Tests/Test1.hs" [] B.empty
+       putStrLn ("code=" ++ show code ++ "\nout=" ++ show out ++ "\nerr=" ++ show err ++ "\n")
+       (code, out, err) <- readProcessWithExitCode "Tests/Test1.hs" [] L.empty
+       putStrLn ("code=" ++ show code ++ "\nout=" ++ show out ++ "\nerr=" ++ show err ++ "\n")
+       (code, out, err) <- readProcessWithExitCode "Tests/Test1.hs" [] T.empty
+       putStrLn ("code=" ++ show code ++ "\nout=" ++ show out ++ "\nerr=" ++ show err ++ "\n")
+       (code, out, err) <- readProcessWithExitCode "Tests/Test1.hs" [] LT.empty
+       putStrLn ("code=" ++ show code ++ "\nout=" ++ show out ++ "\nerr=" ++ show err ++ "\n")
+       (code, out, err) <- readProcessWithExitCode "Tests/Test1.hs" [] ""
        putStrLn ("code=" ++ show code ++ "\nout=" ++ show out ++ "\nerr=" ++ show err)
