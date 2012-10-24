766ed498ea6f89d5f9bafae28a8c3ab6f536760f
David Fox <dsf@seereason.com>
2012-10-24 21:47:09 +0000

    Version 0.4.7 - add compatibility modules

---
+++ System/Process/Text.hs
@@ -0,0 +1,20 @@
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+module System.Process.Text
+    ( readProcess
+    , readProcessWithExitCode
+    , readModifiedProcess
+    , readModifiedProcessWithExitCode
+    ) where
+
+import System.Exit (ExitCode)
+import System.Process (CmdSpec, CreateProcess)
+import qualified System.Process.Read as R
+
+readProcess :: R.Chars a => FilePath -> [String] -> a -> IO a
+readProcess = R.readProcess
+readProcessWithExitCode :: R.Chars a => FilePath -> [String] -> a -> IO (ExitCode, a, a)
+readProcessWithExitCode = R.readProcessWithExitCode
+readModifiedProcess :: R.Chars a => (CreateProcess -> CreateProcess) -> CmdSpec -> a -> IO a
+readModifiedProcess = R.readModifiedProcess
+readModifiedProcessWithExitCode :: (CreateProcess -> CreateProcess) -> R.Chars a => CmdSpec -> a -> IO (ExitCode, a, a)
+readModifiedProcessWithExitCode = R.readModifiedProcessWithExitCode
