87be5212963c6ae5eef54fe47dae9833f118f6e3
David Fox <dsf@seereason.com>
2013-10-15 14:41:01 +0000

    Version 0.6.2 - make changelog visible in hackage

--- Setup.hs
+++ Setup.hs
@@ -2,9 +2,11 @@ import Distribution.Simple
 import Distribution.Simple.LocalBuildInfo (LocalBuildInfo(buildDir))
 import Distribution.Simple.Program
 import System.Cmd
+import System.Directory
 import System.Exit
 
-main = defaultMainWithHooks simpleUserHooks {
+main = copyFile "debian/changelog" "changelog" >>
+       defaultMainWithHooks simpleUserHooks {
          postBuild =
              \ _ _ _ lbi ->
                  case buildDir lbi of
