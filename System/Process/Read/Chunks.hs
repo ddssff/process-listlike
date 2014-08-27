30998d36d66bcace574336ed03d0dd5f6fa02be9
David Fox <dsf@seereason.com>
2014-08-27 16:39:24 -0700

    Add an NFData instance for ExitCode so readProcessChunks can be forced

--- System/Process/Read/Chunks.hs
+++ System/Process/Read/Chunks.hs
@@ -3,9 +3,14 @@ module System.Process.Read.Chunks
     , readProcessChunks
     ) where
 
+import Control.DeepSeq (NFData)
 import System.Exit (ExitCode)
 import System.Process (CreateProcess)
 import System.Process.Read.Interleaved
+
+-- | This lets us use deepseq's force on the stream of data returned
+-- by readProcessChunks.
+instance NFData ExitCode
 
 data Output a = Stdout a | Stderr a | Result ExitCode | Exception IOError deriving (Eq, Show)
 
