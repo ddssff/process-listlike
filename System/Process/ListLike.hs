d8618bd4e3c6b333a8b579872907932acffbaa61
David Fox <dsf@seereason.com>
2014-09-24 14:29:25 -0700

    Export shell and proc from System.Process.ListLike

--- System/Process/ListLike.hs
+++ System/Process/ListLike.hs
@@ -2,9 +2,12 @@
 -- process-listlike package and the ListLikePlus instances.
 module System.Process.ListLike (
   module System.Process.ListLike.Class,
-  module System.Process.ListLike.Chunks
+  module System.Process.ListLike.Chunks,
+  System.Process.proc,
+  System.Process.shell
   ) where
 
+import System.Process (proc, shell)
 import System.Process.ListLike.Class
 import System.Process.ListLike.Instances ()
 import System.Process.ListLike.Chunks
