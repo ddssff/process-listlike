f1befb5c72e337ef23202f03afb830d4ce3d1986
David Fox <dsf@seereason.com>
2014-09-27 14:35:42 -0700

    Flush stderr after writing to it in ePutStrLn

--- Debug/Console.hs
+++ Debug/Console.hs
@@ -8,7 +8,7 @@ module Debug.Console
 import Control.Monad.Trans (MonadIO, liftIO)
 import Control.Concurrent (forkIO, newEmptyMVar, putMVar, takeMVar)
 import Data.Monoid ((<>))
-import System.IO (hPutStr, stderr)
+import System.IO (hPutStr, stderr, hFlush)
 import System.IO.Unsafe (unsafePerformIO)
 
 {-# NOINLINE console #-}
@@ -17,7 +17,7 @@ console = unsafePerformIO $ do
   v <- newEmptyMVar
   _tid <- forkIO (loop v)
   return (putMVar v)
-    where loop v = takeMVar v >>= hPutStr stderr >> loop v
+    where loop v = takeMVar v >>= hPutStr stderr >> hFlush stderr >> loop v
 
 ePutStr :: MonadIO m => String -> m ()
 ePutStr s = liftIO $ console s
